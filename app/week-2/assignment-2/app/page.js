e7ab84c966a283fd362144eff0df05d46f036413
salahudeen781 <61598552+salahudeen781@users.noreply.github.com>
2024-09-22 20:21:05 -0700

    week 2 - assignment
    Find the update of the week 2 assignment based on the expected output in D2l.

---
+++ app/week-2/assignment-2/app/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./week-2/studentInfo";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo></StudentInfo>
+    </div>
+  );
+}
---
+++ app/week-2/assignment-2/app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./studentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
